791f15f6984672fa76f8da08f6dfb764b847ae2a
Amit Pore <development@amitpore.com>
2015-05-21 00:44:05 +0100

    Contact me , email address updated

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'mailing@amitpore.com.com';
+$to = 'mailing@amitpore.com';
 $email_subject = "Amit Pore Blog Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: accounts@amitpore.com\n";
+$headers = "From: accounts@amitpore.com";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
